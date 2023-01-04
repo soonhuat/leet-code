3f9d8e730b1076687961923ed1a94ac131cb287f
Phan Soon Huat <soonhuat.phan@hotmail.com>
2023-01-04 12:29:05 +0000

    Sync LeetCode submission - Longest Palindromic Substring (javascript)

--- problems/longest_palindromic_substring/solution.js
+++ problems/longest_palindromic_substring/solution.js
@@ -3,68 +3,28 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  let wyw = ''
-  const repeatChar = {}
-
-  const pushToDict = (char, index) => {
-    if (repeatChar[char]) {
-      repeatChar[char].push(index)
-    } else {
-      repeatChar[char] = [index]
-    }
-  }
-
-  const reverseString = (str) => {
-    return str.split('').reverse().join('')
-  }
-
+  let palindrome = ''
+  let halfPalindromeLength = 0
   const length = s.length
-  if (length == 1) {
-    return s
-  }
 
-  for (const i in s) {
-    const char = s[i]
-    if (repeatChar[char]) {
-      continue
+  const findPalindrome = (left, right) => {
+    while (left > -1 && right < length && s[left] == s[right]) {
+      left--
+      right++
     }
-    let index = s.indexOf(char, ~~i + 1)
-    if (index != -1) {
-      pushToDict(char, i)
-      pushToDict(char, index)
-      while (index < length) {
-        const nextIndex = s.indexOf(char, index + 1)
-        if (nextIndex != -1) {
-          pushToDict(char, nextIndex)
-          index = nextIndex
-        } else {
-          index = length + 1
-        }
-      }
+    const substr = s.substring(left + 1, right)
+    if (substr.length > palindrome.length) {
+      palindrome = substr
+      halfPalindromeLength = Math.floor(substr.length / 2)
     }
   }
 
-  for (const key in repeatChar) {
-    const charset = repeatChar[key]
-    if (charset.length > 1) {
-      for (const index in charset) {
-        const start = Number(index)
-        if (wyw.length > charset[charset.length - 1] - charset[index]) {
-          break
-        }
-        for (let j = charset.length - 1; j > start; j--) {
-          const substr = s.substring(charset[index], charset[j] + 1)
-          const reversed = reverseString(substr)
-          if (substr == reversed && substr.length > wyw.length) {
-            wyw = substr
-          }
-
-          if (wyw.length > charset[j] - charset[index]) {
-            break
-          }
-        }
-      }
+  for (let i = 0; i < length; i++) {
+    if (length - i < halfPalindromeLength) {
+      break
     }
+    findPalindrome(i, i)
+    findPalindrome(i, i + 1)
   }
-  return wyw || s[0]
+  return palindrome
 };
